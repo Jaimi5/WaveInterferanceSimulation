95a29191474ef369ff0b6c15e8c8b3fd9e1f8c36
Jaimi5 <janms2009@gmail.com>
2020-05-21 00:25:23 +0200

    Some more formulas

--- WaveSimulatorV2/Form1.Designer.cs
+++ WaveSimulatorV2/Form1.Designer.cs
@@ -23,14 +23,14 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.lambdatext = new System.Windows.Forms.Label();
             this.dist = new System.Windows.Forms.Label();
-            this.Wavelenght = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.WavelenghtValue = new System.Windows.Forms.TextBox();
+            this.DistanceSlits = new System.Windows.Forms.TextBox();
             this.NumberOfSlitstext = new System.Windows.Forms.Label();
             this.SlitsWidth = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -38,6 +38,8 @@
             this.SlitsNumber = new System.Windows.Forms.NumericUpDown();
             this.LightNumber = new System.Windows.Forms.NumericUpDown();
             this.LightNumberText = new System.Windows.Forms.Label();
+            this.Distance = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.SlitsNumber)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.LightNumber)).BeginInit();
@@ -45,7 +47,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1749, 376);
+            this.button1.Location = new System.Drawing.Point(1748, 424);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(135, 56);
             this.button1.TabIndex = 0;
@@ -71,19 +73,19 @@
             this.dist.TabIndex = 2;
             this.dist.Text = "Distance between slits";
             // 
-            // Wavelenght
+            // WavelenghtValue
             // 
-            this.Wavelenght.Location = new System.Drawing.Point(1760, 270);
-            this.Wavelenght.Name = "Wavelenght";
-            this.Wavelenght.Size = new System.Drawing.Size(110, 20);
-            this.Wavelenght.TabIndex = 3;
+            this.WavelenghtValue.Location = new System.Drawing.Point(1760, 270);
+            this.WavelenghtValue.Name = "WavelenghtValue";
+            this.WavelenghtValue.Size = new System.Drawing.Size(110, 20);
+            this.WavelenghtValue.TabIndex = 3;
             // 
-            // textBox1
+            // DistanceSlits
             // 
-            this.textBox1.Location = new System.Drawing.Point(1760, 163);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(110, 20);
-            this.textBox1.TabIndex = 4;
+            this.DistanceSlits.Location = new System.Drawing.Point(1760, 163);
+            this.DistanceSlits.Name = "DistanceSlits";
+            this.DistanceSlits.Size = new System.Drawing.Size(110, 20);
+            this.DistanceSlits.TabIndex = 4;
             // 
             // NumberOfSlitstext
             // 
@@ -112,19 +114,20 @@
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(1686, 999);
             this.chart1.TabIndex = 9;
             this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
             // 
             // SlitsNumber
             // 
@@ -159,11 +162,29 @@
             this.LightNumberText.TabIndex = 11;
             this.LightNumberText.Text = "Number of Lights";
             // 
+            // Distance
+            // 
+            this.Distance.Location = new System.Drawing.Point(1760, 376);
+            this.Distance.Name = "Distance";
+            this.Distance.Size = new System.Drawing.Size(110, 20);
+            this.Distance.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1757, 360);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Distance to the Screen";
+            // 
             // WaveInterferanceSimulator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.Distance);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.LightNumber);
             this.Controls.Add(this.LightNumberText);
             this.Controls.Add(this.SlitsNumber);
@@ -171,8 +192,8 @@
             this.Controls.Add(this.SlitsWidth);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.NumberOfSlitstext);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.Wavelenght);
+            this.Controls.Add(this.DistanceSlits);
+            this.Controls.Add(this.WavelenghtValue);
             this.Controls.Add(this.dist);
             this.Controls.Add(this.lambdatext);
             this.Controls.Add(this.button1);
@@ -193,8 +214,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label lambdatext;
         private System.Windows.Forms.Label dist;
-        private System.Windows.Forms.TextBox Wavelenght;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox WavelenghtValue;
+        private System.Windows.Forms.TextBox DistanceSlits;
         private System.Windows.Forms.Label NumberOfSlitstext;
         private System.Windows.Forms.TextBox SlitsWidth;
         private System.Windows.Forms.Label label1;
@@ -202,6 +223,8 @@
         private System.Windows.Forms.NumericUpDown SlitsNumber;
         private System.Windows.Forms.NumericUpDown LightNumber;
         private System.Windows.Forms.Label LightNumberText;
+        private System.Windows.Forms.TextBox Distance;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WaveSimulatorV2/Form1.cs
+++ WaveSimulatorV2/Form1.cs
@@ -19,6 +19,8 @@ namespace WaveSimulatorV2 {
 
         }
 
+        private double Precision;
+
         private void button1_Click(object sender, EventArgs e) {
             var chart = chart1.ChartAreas[0];
             chart.AxisX.IntervalType = DateTimeIntervalType.Number;
@@ -28,23 +30,74 @@ namespace WaveSimulatorV2 {
             chart.AxisY.LabelStyle.IsEndLabelVisible = true;
             //chart.AxisX.Maximum = 100; If is need to put maximum
             chart.AxisX.Interval = 1;
-            chart.AxisY.Interval = 1;
+            chart.AxisY.Interval = Math.Pow(10, -23);
+            if (!chart1.Series.Any(x => x.Name == "Interference")) {
+                chart1.Series.Add("Interference");
+                chart1.Series["Interference"].ChartType = SeriesChartType.FastLine;
+                chart1.Series["Interference"].Color = Color.Black;
+                chart1.Series[0].IsVisibleInLegend = false;
+            }
 
-            chart1.Series.Add("Interference");
-            chart1.Series["Interference"].ChartType = SeriesChartType.FastLine; 
-            chart1.Series["Interference"].Color = Color.Black;
-            chart1.Series[0].IsVisibleInLegend = false;
+            int numberofSlits = (int)SlitsNumber.Value;
+            double wavelenght = double.Parse(WavelenghtValue.Text) * Math.Pow(10, -10); //Amstrongs
+            double amplitude = GetAmplitude(wavelenght);
+            double distancetoScreen = double.Parse(Distance.Text) * Math.Pow(10, -3); //milimeters
+            double slitsWidth = double.Parse(SlitsWidth.Text) * Math.Pow(10, -3); //milimeters
 
-            
+            Precision = wavelenght / 2;
+            List<double> vector = GetVectorSlits(numberofSlits, slitsWidth);
+            chart1.Series["Interference"].Points.Clear();
+            for (int i = -60000; i<= 60000; i++)
+                chart1.Series["Interference"].Points.AddXY(i, GetElectricFieldOfaPoint(i, distancetoScreen, amplitude, wavelenght, numberofSlits, vector));
 
-            for(int i = 0; i<30000; i++)
-                chart1.Series["Interference"].Points.AddXY(i, Math.Sin(i));
-            double j = 30000;
-            for (int i = 30000; i<60000; i++, j--)
-                chart1.Series["Interference"].Points.AddXY(i,Math.Sin(j) ); 
+        }
+
+
+
+        private double GetDistance(int r, double distancetoScreen, double distanceToCenter) {
+            return Math.Sqrt(Math.Pow(distanceToCenter - r * Precision, 2) + Math.Pow(distancetoScreen, 2));
+        }
+
+        private double GetElectricFieldOfaPoint(int r, double distancetoScreen, double amplitude, double wavelenght, int numberofSlits, List<double> distanceVector) {
+            int M = 10;
+            double result = 0;
+            for (int j = 0; j < numberofSlits; j++) {
+                double mitjana = 0;
+                double distance = GetDistance(r, distancetoScreen, distanceVector[j]);
+                for (int i = 1; i < M; i++)
+                    mitjana += (amplitude / distance) * Math.Cos(2 * Math.PI * ((distance / wavelenght) - (i / M)));
+                result += Math.Pow(mitjana/M, 2);
+            }
+            return result;
+        }
+
+        private double GetAmplitude(double waveLenght) {
+            return waveLenght / (2 * Math.PI);
+        }
+
+        private List<double> GetVectorSlits(int numberSlits, double distanceSlits) {
+            List<double> slitsPosition = new List<double>();
+
+            for (int i = numberSlits / 2 - 1; i >= 0; i--) {
+                if (numberSlits % 2 == 0)
+                    slitsPosition.Add(distanceSlits / 2 + distanceSlits * i);
+                else
+                    slitsPosition.Add(distanceSlits * i + distanceSlits);
+            }
+            for (int i = 0; i < numberSlits / 2; i++)
+                slitsPosition.Add(-slitsPosition[i]);
+
+            if (numberSlits % 2 != 0)
+                slitsPosition.Add(0);
+
+            return slitsPosition;
         }
 
         private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e) {
 
         }
     }
