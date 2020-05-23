7332cd691b33ff90a97d1af693b3ab5a79a853f0
Jaimi5 <janms2009@gmail.com>
2020-05-23 12:04:34 +0200

    Formula Changes and others

--- WaveSimulatorV2/Form1.Designer.cs
+++ WaveSimulatorV2/Form1.Designer.cs
@@ -23,9 +23,9 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.lambdatext = new System.Windows.Forms.Label();
             this.dist = new System.Windows.Forms.Label();
@@ -40,6 +40,7 @@
             this.LightNumberText = new System.Windows.Forms.Label();
             this.Distance = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.ClearChart = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.SlitsNumber)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.LightNumber)).BeginInit();
@@ -114,16 +115,16 @@
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(1686, 999);
             this.chart1.TabIndex = 9;
             this.chart1.Text = "chart1";
@@ -178,11 +179,22 @@
             this.label2.TabIndex = 13;
             this.label2.Text = "Distance to the Screen";
             // 
+            // ClearChart
+            // 
+            this.ClearChart.Location = new System.Drawing.Point(1748, 555);
+            this.ClearChart.Name = "ClearChart";
+            this.ClearChart.Size = new System.Drawing.Size(135, 56);
+            this.ClearChart.TabIndex = 15;
+            this.ClearChart.Text = "ClearChart";
+            this.ClearChart.UseVisualStyleBackColor = true;
+            this.ClearChart.Click += new System.EventHandler(this.ClearChart_Click);
+            // 
             // WaveInterferanceSimulator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.ClearChart);
             this.Controls.Add(this.Distance);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.LightNumber);
@@ -225,6 +237,7 @@
         private System.Windows.Forms.Label LightNumberText;
         private System.Windows.Forms.TextBox Distance;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ClearChart;
     }
 }
 
--- WaveSimulatorV2/Form1.cs
+++ WaveSimulatorV2/Form1.cs
@@ -25,10 +25,11 @@ namespace WaveSimulatorV2 {
             var chart = chart1.ChartAreas[0];
             chart.AxisX.IntervalType = DateTimeIntervalType.Number;
 
-            chart.AxisX.LabelStyle.Format = "";
-            chart.AxisY.LabelStyle.Format = "";
-            chart.AxisY.LabelStyle.IsEndLabelVisible = true;
-            //chart.AxisX.Maximum = 100; If is need to put maximum
+            //chart1.ChartAreas[0].AxisY.MajorGrid.Interval = 3; 
+            //chart1.ChartAreas[0].AxisY.MajorGrid.IntervalOffset = 2;  
+            //chart1.ChartAreas[0].AxisY.MajorTickMark.Interval = 0.2; 
+            //chart1.ChartAreas[0].AxisY.MajorTickMark.IntervalOffset = 0.5;            
+
             chart.AxisX.Interval = 1;
             chart.AxisY.Interval = Math.Pow(10, -23);
             if (!chart1.Series.Any(x => x.Name == "Interference")) {
@@ -36,19 +37,24 @@ namespace WaveSimulatorV2 {
                 chart1.Series["Interference"].ChartType = SeriesChartType.FastLine;
                 chart1.Series["Interference"].Color = Color.Black;
                 chart1.Series[0].IsVisibleInLegend = false;
+                chart1.Series["Interference"].MarkerStep = 10;
+
             }
 
             int numberofSlits = (int)SlitsNumber.Value;
             double wavelenght = double.Parse(WavelenghtValue.Text) * Math.Pow(10, -10); //Amstrongs
-            double amplitude = GetAmplitude(wavelenght);
+            //double amplitude = GetAmplitude(wavelenght);
             double distancetoScreen = double.Parse(Distance.Text) * Math.Pow(10, -3); //milimeters
-            double slitsWidth = double.Parse(SlitsWidth.Text) * Math.Pow(10, -3); //milimeters
+            double amplitude = double.Parse(SlitsWidth.Text) * Math.Pow(10, -3); //milimeters
+            double DistanceBetweenSlits = double.Parse(DistanceSlits.Text) * Math.Pow(10, -3); //milimeters
 
-            Precision = wavelenght / 2;
-            List<double> vector = GetVectorSlits(numberofSlits, slitsWidth);
-            chart1.Series["Interference"].Points.Clear();
-            for (int i = -60000; i<= 60000; i++)
-                chart1.Series["Interference"].Points.AddXY(i, GetElectricFieldOfaPoint(i, distancetoScreen, amplitude, wavelenght, numberofSlits, vector));
+            Precision = wavelenght / 10;
+            List<double> vector = GetVectorSlits(numberofSlits, DistanceBetweenSlits);
+            //chart1.Series["Interference"].Points.Clear();
+            for (int i = -10000; i <= 10000; i++) {
+                var aux = GetElectricFieldOfaPoint(i, distancetoScreen, amplitude, wavelenght, numberofSlits, vector);
+                chart1.Series["Interference"].Points.AddXY(i, aux);
+            }
 
         }
 
@@ -66,7 +72,7 @@ namespace WaveSimulatorV2 {
                 double distance = GetDistance(r, distancetoScreen, distanceVector[j]);
                 for (int i = 1; i < M; i++)
                     mitjana += (amplitude / distance) * Math.Cos(2 * Math.PI * ((distance / wavelenght) - (i / M)));
-                result += Math.Pow(mitjana/M, 2);
+                result += Math.Pow(mitjana / M, 2);
             }
             return result;
         }
@@ -99,6 +105,10 @@ namespace WaveSimulatorV2 {
 
         private void chart1_Click(object sender, EventArgs e) {
 
+        }
+
+        private void ClearChart_Click(object sender, EventArgs e) {
+            chart1.Series["Interference"].Points.Clear();
         }
     }
 }
